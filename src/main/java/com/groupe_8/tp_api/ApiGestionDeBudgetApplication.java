6ff1d57a25a21000996b15f2c2da84cd9374f697
adama konake <adamakoanke@gmail.com>
2023-08-04 10:13:12 +0000

    envoie du project sur git

---
+++ src/main/java/com/groupe_8/tp_api/ApiGestionDeBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.groupe_8.tp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionDeBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionDeBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe_8/tp_api/ApiGestionDeBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe_8.tp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionDeBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
