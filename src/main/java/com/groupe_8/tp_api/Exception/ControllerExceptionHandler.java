4ece2dbeb4c04ce6e267e0746c237c5b97581aa3
adama konake <adamakoanke@gmail.com>
2023-08-05 11:05:28 +0000

    ajout de commentaire

--- src/main/java/com/groupe_8/tp_api/Exception/ControllerExceptionHandler.java
+++ src/main/java/com/groupe_8/tp_api/Exception/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package com.groupe_8.tp_api.Exception;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -53,6 +55,21 @@ public class ControllerExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 new Date(),
                 ex.getMessage(),
+                request.getDescription(false)
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request){
+        final String[] messageError = {""};
+        ex.getBindingResult().getAllErrors().forEach( (error) -> {
+            messageError[0] += "'"+((FieldError) error).getField()+"' : "+error.getDefaultMessage()+", ";
+        });
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                messageError[0],
                 request.getDescription(false)
         );
     }
