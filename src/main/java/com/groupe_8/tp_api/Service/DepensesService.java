2e101a6a40b5ce01f39a5d36914658ae7d122962
Muhammad23 <md9624561@gmail.com>
2023-08-07 15:55:56 +0000

    commit

--- src/main/java/com/groupe_8/tp_api/Service/DepensesService.java
+++ src/main/java/com/groupe_8/tp_api/Service/DepensesService.java
@@ -1,6 +1,9 @@
 package com.groupe_8.tp_api.Service;
 
+import com.groupe_8.tp_api.Exception.NoContentException;
+import com.groupe_8.tp_api.Model.Budget;
 import com.groupe_8.tp_api.Model.Depenses;
+import com.groupe_8.tp_api.Repository.BudgetRepository;
 import com.groupe_8.tp_api.Repository.DepensesRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,12 +16,18 @@ import java.util.List;
 @AllArgsConstructor
 public class DepensesService {
     private final DepensesRepository depensesRepository;
+    private  final BudgetRepository budgetRepository;
     public Depenses creer(Depenses depenses){
+
         depenses.setDate(LocalDate.now());
         return depensesRepository.save(depenses);
     }
     public List<Depenses> lire(){
-        return depensesRepository.findAll();
+        List<Depenses> depensesList = depensesRepository.findAll();
+        if (depensesList.isEmpty())
+            throw new NoContentException("Aucune depenses trouvée");
+        return depensesList;
+
     }
     public Depenses modifier(long id, Depenses depenses){
         return depensesRepository.findById(id)
