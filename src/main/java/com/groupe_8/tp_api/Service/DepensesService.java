d3fc5135717fdd3e446acb424908906531abdfa3
Muhammad23 <md9624561@gmail.com>
2023-08-07 20:04:31 +0000

    commit

--- src/main/java/com/groupe_8/tp_api/Service/DepensesService.java
+++ src/main/java/com/groupe_8/tp_api/Service/DepensesService.java
@@ -1,11 +1,12 @@
 package com.groupe_8.tp_api.Service;
 
+import com.groupe_8.tp_api.Exception.BadRequestException;
 import com.groupe_8.tp_api.Exception.NoContentException;
-import com.groupe_8.tp_api.Model.Budget;
-import com.groupe_8.tp_api.Model.Depenses;
+import com.groupe_8.tp_api.Model.*;
 import com.groupe_8.tp_api.Repository.BudgetRepository;
 import com.groupe_8.tp_api.Repository.DepensesRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -15,11 +16,50 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class DepensesService {
-    private final DepensesRepository depensesRepository;
-    private  final BudgetRepository budgetRepository;
+    @Autowired
+    private  DepensesRepository depensesRepository;
+    @Autowired
+    private   BudgetRepository budgetRepository;
+    @Autowired
+    private  BudgetService budgetService;
     public Depenses creer(Depenses depenses){
+        LocalDate dateDepenses = depenses.getDate();
+        Categorie categorie = depenses.getCategorie();
+        Utilisateur user = depenses.getUtilisateur();
+        Type type = depenses.getType();
+        Depenses depensesVerif = null;
+        Budget budget = budgetRepository.findByUtilisateurAndCategorieAndDateFinIsAfter(user,categorie,LocalDate.now());
+        if(dateDepenses.isBefore(budget.getDateDebut()) || dateDepenses.isAfter(LocalDate.now()))
+            throw new BadRequestException("Entrez une date correcte");
 
-        depenses.setDate(LocalDate.now());
+        switch (type.getTitre()){
+            case "quotidient" :
+                depensesVerif = depensesRepository.findByUtilisateurAndTypeAndDate(user,type,dateDepenses);
+                if (depensesVerif != null)
+                    throw  new BadRequestException("Desole vous avez deja effectué votre depenses journilière de " +categorie.getTitre());
+
+                budgetService.updateMontantRestant(depenses);
+                break;
+            case  "hebdomadaire" :
+                depensesVerif = depensesRepository.findFirstByUtilisateurAndCategorieAndTypeOrderByDateDesc(user,
+                        categorie,type);
+                if (depensesVerif.getDate().plusDays(7).isAfter(LocalDate.now()))
+                    throw  new BadRequestException("Desole vous avez deja effectué votre depenses hebdomadaire de " +categorie.getTitre());
+
+                budgetService.updateMontantRestant(depenses);
+                break;
+            case "mensuelle" :
+                depensesVerif = depensesRepository.findFirstByUtilisateurAndCategorieAndTypeOrderByDateDesc(user,
+                        categorie,type);
+                if (depensesVerif.getDate().plusDays(30).isAfter(LocalDate.now()))
+                    throw  new BadRequestException("Desole vous avez deja effectué votre depenses mensuelle de " +categorie.getTitre());
+
+                budgetService.updateMontantRestant(depenses);
+                break;
+            default:
+                throw  new BadRequestException("Ce type de depense n'existe pas");
+
+        }
         return depensesRepository.save(depenses);
     }
     public List<Depenses> lire(){
